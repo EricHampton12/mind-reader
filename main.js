6c086a4ba1b80ebf9aaecc5262d3989e5eb681f6
Eric Hampton <erichampton2010@gmail.com>
2019-09-15 23:39:06 -0400

    final

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 var currentState = 0;
-var states = ['home', 'page1', 'page2', 'page3', 'page4'];
-var symbols = ['!', '@', '#', '$', '%', '^', '&'];
-
-resetButton = document.getElementById('resetButton');
-resetButton.addEventListener('click', resetState);
-document.getElementById('resetButton').style.display = 'none';
+var states = ['home', 'page1', 'page2', 'page3', 'page4', 'page5'];
+var symbols9 = ["!", "@", "#", "$", "%", "&", "&"];
+var smallText = document.querySelector('.Example');
+var pText = document.getElementById('pText');
+var smallText = document.getElementById('smallText');
 var randomsymbols9;
-nextButton = document.getElementById('nextButton');
+var nextBtn = document.getElementById('nextButton');
+var resetBtn = document.getElementById('resetButton');
 nextButton.addEventListener('click', changeState);
+resetButton.addEventListener('click', resetState);
 
 function resetState() {
     currentState = 0;
@@ -19,50 +20,57 @@ function changeState() {
     renderState();
 }
 
+console.log(states[currentState]);
+
+
 function renderState() {
 
-    switch(currentState) {
-        case 'home':
-            document.getElementById('nextButton').style.display = 'inital';
-            document.getElementById('pText').innerHTML ='Pick a number between 01-99';
-            document.getElementById('nextButton').innerHTML = 'Next';
-            document.getElementById('resetButton').style.display = 'none';
-            document.getElementById('resetButton').innerHTML = 'Reset';
-            document.getElementById('smallText').innerHTML = 'When you have chosen a number, click next.';
+    switch (states[currentState]) {
+
+        case 'home': 
+            pText.textContent = "I can read your mind";
+            smallText.textContent = "Press next to continue";
+            resetBtn.style.visibility = 'hidden';
             break;
 
-        case 'page1':
-            document.getElementById('resetButton').style.display = 'initial';
-            document.getElementById('pText').innerHTML = 'Add both digits together to get a new number';
-            document.getElementById('smallText').innerHTML = 'Example: 21 is 2 + 1 = 3';
+        case 'page1': 
+            pText.textContent = "Pick a number between 01-99";
+            smallText.textContent = "When you have picked your number, click next";
             break;
 
         case 'page2':
-            document.getElementById('pText').innerHTML = 'Subtract your new number from the original number';
-            document.getElementById('smallText').innerHTML = 'Example: 21 - 3 = 18';
+            pText.textContent = "Add both digits together to get a new number";
+            smallText.textContent = "Ex: 14 is 1 + 4 = 5";
             break;
 
         case 'page3':
-            document.getElementById('pText').innerHTML = '';
-            randomsymbols9 = symbols9[Math.floor(Math.random() * symbols9.length)];
-            for(let i = 0; i <= 99; i++) {
-                if (i % 9 === 0) {
-                    document.getElementById('pText').innerHTML += i + '-' + randomsybols9 + '<br';
-                
-                } else {
-                    var symbols = [''];
-                    let randomSymbols = symbols[Math.floor(Math.random() * symbols.length)];
-                }
-            }
-                document.getElementById('smallText').innerHTML = 'Find your new number and remember the symbol beside your number.';
-                document.getElementById('nextButton').innerHTML = 'Reveal';
-
-        case 'page4':
-            document.getElementById('nextButton').style.display = 'none';
-            document.getElementById('pText').innerHTML = randomsybols9;
-            document.getElementById('smallText').innerHTML = 'Your symbol is :<br><br>' + randomsymbols9;
+            pText.textContent = "Subtract your new number from the original number";
+            smallText.textContent = "Ex: 14 - 5 = 9";
             break;
-
-
+        
+        case 'page4':
+            pText.innerHTML = " ";
+            randomsymbols9 = symbols9[Math.floor(Math.random()* symbols9.length)];
+            for (var i = 0 ;i < 100; i++) {
+                if (i % 9 == 0) {
+                    pText.innerHTML += i + " - " + randomsymbols9 + "<br>";
+                }
+                else  {
+                    let symbols = ['@', '&', '%', '*'];
+                    let randomSymbols = symbols[Math.floor(Math.random() * symbols.length)];
+                    var char = String.fromCharCode(Math.floor(Math.random() * 10) + 36);
+                    pText.innerHTML += i + " = " + char + "<br>";
+            }
         }
+            nextBtn.style.visibility = "visible";
+            smallText.innerHTML = "Find your new number";
+            break;
+        case 'page5':
+            pText.innerHTML = randomsymbols9;
+            smallText.innerHTML = "Your symbol is :<br><br>" + randomsymbols9;
+            resetBtn.style.visibility = "visible";
+            nextBtn.style.visibility = "invisible";
+        break;
+    }
 }
+
